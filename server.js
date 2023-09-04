40936012cff06edd9bc226cdc5ce3a0ebd834d29
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-03 21:27:33 -0400

    node server

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+//common core moduels inports
+const http = require("http");
+const path = require("path"); //file path
+const fs = require("fs"); //file system
+const fsPromises = require("fs").promises;
+
 const logEvents = require("./logEvents");
-
 const eventEmitter = require("events");
-
-class MyEmitter extends eventEmitter {}
-
+class emitter extends eventEmitter {}
 //init object
-const myEmitter = new MyEmitter();
+const myEmitter = new emitter();
+//port for webserver
+const PORT = process.env.PORT || 3500;
 
+// minum server
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  const extention = path.extname(req.url);
+  let contentType;
+});
+//server lisent to request
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //add a listener the log event
-myEmitter.on("log", (msg) => logEvents(msg));
-
-setTimeout(() => {
-  //emit event
-  myEmitter.emit("log", "Log event Emitted");
-}, 2000);
+// myEmitter.on("log", (msg) => logEvents(msg));
+// //emit event
+// myEmitter.emit("log", "Log event Emitted");
+//1.35.16
